109b33a0349300d417de4e110d5243521dd32ee9
Hayden <hayden26@live.com>
2023-11-12 17:24:58 +0400

    Inital files

---
+++ lessons.js
@@ -0,0 +1,65 @@
+let lessons = [
+    {
+        "id": 1,
+        "subject": "Football",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 20.00,
+        "image": "images/football.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 2,
+        "subject": "Tennis",
+        "location": "Block 16, Knowledge Park, Dubai",
+        "price": 10.00,
+        "image": "images/tennis.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 3,
+        "subject": "Cricket",
+        "location": "Block 17, Knowledge Park, Dubai",
+        "price": 30.00,
+        "image": "images/cricket.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 4,
+        "subject": "Basketball",
+        "location": "Block 17, Knowledge Park, Dubai",
+        "price": 20.00,
+        "image": "images/basketball.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 5,
+        "subject": "Swimming",
+        "location": "Block 19, Knowledge Park, Dubai",
+        "price": 40.00,
+        "image": "images/swimming.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 6,
+        "subject": "Volleyball",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 25.00,
+        "image": "images/volleyball.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 7,
+        "subject": "Table Tennis",
+        "location": "Block 16, Knowledge Park, Dubai",
+        "price": 15.00,
+        "image": "images/table-tennis.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+]
